5e744893052e2ff7b76489b45ac19859b6e25aba
brenoSBC <sbcbreno01@gmail.com>
2026-05-22 18:19:33 -0300

    delete archive

--- src/protocol/query.c
+++
@@ -1,8 +0,0 @@
-#include "../../include/query.h"
-
-/* Merge Header and Question */
-void dns_build_query(unsigned char *buffer, unsigned char *header, int header_size, unsigned char *question, int question_size) {
-
-    memcpy(buffer, header, header_size);
-    memcpy(buffer + header_size, question, question_size);
-}
